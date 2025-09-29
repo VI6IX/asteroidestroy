d68d0d5fc71f6224bae764def7e7f3383eaaeb5c
Victor <deVictorNeves@gmail.com>
2025-09-29 10:24:07 -0300

    refactor: screen wrap is based on viewport size

--- scripts/wrap_component.gd
+++ scripts/wrap_component.gd
@@ -1,24 +1,23 @@
 extends Node2D
 
 @export var COMPONENT_PARENT : Node2D;
-
 @onready var SCREEN_SIZE = get_viewport_rect().size;
 
 func horizontal_wrap():
-	if COMPONENT_PARENT.position.x > (1152.0 + COMPONENT_PARENT.scale.x):
+	if COMPONENT_PARENT.position.x > (SCREEN_SIZE.x + COMPONENT_PARENT.scale.x):
 		print(str(COMPONENT_PARENT.name) + " has passed the right screen boundary");
-		COMPONENT_PARENT.position.x = -1151.0;
-	if COMPONENT_PARENT.position.x < -(1152.0 - COMPONENT_PARENT.scale.x):
+		COMPONENT_PARENT.position.x = -SCREEN_SIZE.x + 1;
+	if COMPONENT_PARENT.position.x < -(SCREEN_SIZE.x - COMPONENT_PARENT.scale.x):
 		print(str(COMPONENT_PARENT.name) + " has passed the left screen boundary");
-		COMPONENT_PARENT.position.x = 1151.0;
+		COMPONENT_PARENT.position.x = SCREEN_SIZE.x - 1;
 
 func vertical_wrap():
-	if COMPONENT_PARENT.position.y < -(648.0 + COMPONENT_PARENT.scale.y):
+	if COMPONENT_PARENT.position.y < -(SCREEN_SIZE.y + COMPONENT_PARENT.scale.y):
 		print(str(COMPONENT_PARENT.name) + " has passed the upper screen boundary");
-		COMPONENT_PARENT.position.y = 647.0;
-	if COMPONENT_PARENT.position.y > (648.0 + COMPONENT_PARENT.scale.y):
+		COMPONENT_PARENT.position.y = SCREEN_SIZE.y - 1;
+	if COMPONENT_PARENT.position.y > (SCREEN_SIZE.y + COMPONENT_PARENT.scale.y):
 		print(str(COMPONENT_PARENT.name) + " has passed the lower screen boundary");
-		COMPONENT_PARENT.position.y = -647.0;
+		COMPONENT_PARENT.position.y = -SCREEN_SIZE.y - 1;
 
 func _process(_delta: float) -> void:
 	horizontal_wrap();
