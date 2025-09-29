b2945db62c48d602c5097f73c8445095aa94bd38
Victor <deVictorNeves@gmail.com>
2025-09-28 22:14:24 -0300

    feat: add gradual acceleration to thrust

--- scenes/PlayerInput.gd
+++ scenes/PlayerInput.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @export var Raycast : RayCast2D;
 @export var PAN_SPEED : float = 5;
+@export var ACCELERATION : Vector2;
 @export var THRUST_FORCE : float = 800;
 @export var MAX_SPEED : float = 800;
 @export var DECELERATION_RATE : float = 5;
@@ -20,9 +21,13 @@ func pan(delta) -> void:
 	# PAN_SPEED times delta
 	rotation += PAN_DIRECTION * PAN_SPEED * delta;
 
-func thrust():
+func thrust(delta):
+	ACCELERATION = Vector2.ZERO;
 	if Input.is_action_pressed("thrust"):
-		velocity = transform.x * THRUST_FORCE;
+		ACCELERATION = transform.x * THRUST_FORCE * delta;
+		velocity += ACCELERATION;
+		if velocity.length() >= MAX_SPEED:
+			velocity = velocity.limit_length(MAX_SPEED);
 
 func decelerate():
 	if !Input.is_action_pressed("thrust"):
@@ -37,7 +42,8 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	pan(delta);
-	thrust();
+	thrust(delta);
 	decelerate();
 	shoot();
 	move_and_slide();
+	print("Velocity X = " + str(velocity.x) + " | " + " Velocity Y = "+ str(velocity.y) + " | " + "Velocity Magnitude = " + str(velocity.length()));
