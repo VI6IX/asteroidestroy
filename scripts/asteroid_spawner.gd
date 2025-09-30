5a44a5118d0ff1c688f1b83831e4e8c823dfddf2
Victor <deVictorNeves@gmail.com>
2025-09-30 20:23:44 -0300

    feat: implement asteroid instantiation

--- scripts/asteroid_spawner.gd
+++ scripts/asteroid_spawner.gd
@@ -1,18 +1,22 @@
 extends Node2D
 
 @onready var ASTEROID := preload("res://scenes/asteroid.tscn");
+@onready var TIMER : Timer = $Timer;
 
 @export var asteroid_limit : int = 2;
 @export var asteroid_count : int = 0;
-
 
 func Instantiate_Asteroid():
 	var asteroid_instance = ASTEROID.instantiate();
 	add_child(asteroid_instance);
 	pass
 
-func _ready() -> void:
-	Instantiate_Asteroid();
-
 func _process(_delta: float) -> void:
 	pass
+
+func _on_timer_timeout() -> void:
+	if asteroid_count < asteroid_limit:
+		print("Asteroids can be instantiated.");
+		Instantiate_Asteroid();
+	else:
+		print("Asteroid limit reached: asteroids cannot be instantiated.");
