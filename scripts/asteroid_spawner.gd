afd315d3e2e39595dcf6af51012832f9c49d3d23
Victor <deVictorNeves@gmail.com>
2025-09-30 21:31:04 -0300

    feat: add randomized asteroid instance spawn location

--- scripts/asteroid_spawner.gd
+++ scripts/asteroid_spawner.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
+@onready var PLAYER := $"../Player"
 @onready var ASTEROID := preload("res://scenes/asteroid.tscn");
+@onready var SPAWN_LOCATION := $SpawnPath/SpawnLocation
 @onready var TIMER : Timer = $Timer;
 
 @export var asteroid_limit : int = 2;
@@ -8,6 +10,8 @@ extends Node2D
 
 func Instantiate_Asteroid():
 	var asteroid_instance = ASTEROID.instantiate();
+	SPAWN_LOCATION.progress_ratio = randf();
+	asteroid_instance.position = SPAWN_LOCATION.position;
 	add_child(asteroid_instance);
 	pass
 
@@ -18,5 +22,7 @@ func _on_timer_timeout() -> void:
 	if asteroid_count < asteroid_limit:
 		print("Asteroids can be instantiated.");
 		Instantiate_Asteroid();
+		asteroid_count += 1;
 	else:
 		print("Asteroid limit reached: asteroids cannot be instantiated.");
+		return
