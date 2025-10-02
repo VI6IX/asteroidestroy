3e655f07169a6ab3317f47b3407be50bf777ff47
Victor <deVictorNeves@gmail.com>
2025-10-02 16:54:27 -0300

    refactor: rename PlayerInput.gd to player.gd

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,11 +1,10 @@
 extends CharacterBody2D
 
-@export var BULLET_SPEED : float = 500;
-@export var MAX_SPEED : float = 1000;
+@export var BULLET_SPEED : float = 2000;
 
-func _process(delta: float) -> void:
-	if velocity.length() < MAX_SPEED:
-		velocity += transform.x * BULLET_SPEED * delta;
+func _process(_delta: float) -> void:
+	if velocity.length() < BULLET_SPEED:
+		velocity = transform.x * BULLET_SPEED;
 	else:
-		velocity = velocity.limit_length(MAX_SPEED);
+		velocity = velocity.limit_length(BULLET_SPEED);
 	move_and_slide();
