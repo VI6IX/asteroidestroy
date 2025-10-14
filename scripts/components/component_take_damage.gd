9b1883b30de8bd5b9d8d6e07a1fc3ed163dd5d7c
Victor <deVictorNeves@gmail.com>
2025-10-14 20:57:52 -0300

    refactor: fully remove component_take damage

--- scripts/components/component_take_damage.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-@export var PARENT : Node2D;
-
-func _on_area_entered(area: Area2D) -> void:
-	print(str(PARENT.name) + " has collided with " + str(area.name));
-	PARENT.queue_free();
