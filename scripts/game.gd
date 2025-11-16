620a9dfc0f99916cfd0073b827783fb307cd9d36
Victor <deVictorNeves@gmail.com>
2025-11-16 20:58:56 -0300

    its over

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,3 +3,6 @@ extends Node2D
 func _process(_delta: float) -> void:
 	if Input.is_action_pressed("escape"):
 		get_tree().change_scene_to_file("res://scenes/main_menu.tscn");
+		GLOBAL_VARIABLES.asteroid_limit = 5;
+		GLOBAL_VARIABLES.PLAYER_LIVES = 3;
+		GLOBAL_VARIABLES.score = 0;
