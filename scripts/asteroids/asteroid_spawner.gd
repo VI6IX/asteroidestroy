502add69f7e95f9ee86a99a3874faa5cd67f3c20
Victor <deVictorNeves@gmail.com>
2025-11-16 15:49:17 -0300

    feat: add sound effect to asteroid destruction

--- scripts/asteroids/asteroid_spawner.gd
+++ scripts/asteroids/asteroid_spawner.gd
@@ -2,14 +2,17 @@ extends Node2D
 
 @onready var PLAYER := $"../Player"
 @onready var LARGE_ASTEROID := preload("res://scenes/asteroids/asteroid_large.tscn");
-@export var LARGE_SCORE : int = 50;
 @onready var MEDIUM_ASTEROID := preload("res://scenes/asteroids/asteroid_medium.tscn");
-@export var MEDIUM_SCORE : int = 100;
 @onready var SMALL_ASTEROID := preload("res://scenes/asteroids/asteroid_small.tscn");
-@export var SMALL_SCORE : int = 200;
 @onready var SPAWN_LOCATION := $SpawnPath/SpawnLocation
 @onready var TARGET_POSITION := $TargetPath/TargetPosition
 @onready var TIMER : Timer = $Timer;
+@onready var sfx_destruction: AudioStreamPlayer2D = $sfx_destruction
+
+
+@export var SMALL_SCORE : int = 200;
+@export var LARGE_SCORE : int = 50;
+@export var MEDIUM_SCORE : int = 100;
 
 func spawn_large_asteroid():
 	var asteroid_instance = LARGE_ASTEROID.instantiate();
@@ -23,6 +26,7 @@ func _on_asteroid_destroyed(asteroid): # called on asteroid's component damage h
 	# the asteroid parameter is the health_depleted's argument (the component's parent)
 	var queued_asteroid = null
 	var incremented_rotation = asteroid.rotation
+	sfx_destruction.play();
 	
 	if asteroid.size == asteroid.ASTEROID_SIZE.LARGE:
 		queued_asteroid = MEDIUM_ASTEROID
@@ -33,8 +37,8 @@ func _on_asteroid_destroyed(asteroid): # called on asteroid's component damage h
 		#print("MEDIUM ASTEROID DESTROYED - ADDING " + str(MEDIUM_SCORE) + " TO SCORE!")
 		GLOBAL_VARIABLES.on_score_increment(MEDIUM_SCORE);
 	else:
-		GLOBAL_VARIABLES.on_score_increment(SMALL_SCORE);
 		#print("SMALL ASTEROID DESTROYED - ADDING " + str(SMALL_SCORE) + " TO SCORE!")
+		GLOBAL_VARIABLES.on_score_increment(SMALL_SCORE);
 		despawn(asteroid)
 		return
 	
