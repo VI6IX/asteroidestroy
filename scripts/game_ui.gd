9b5a99604d34a2a197ad88097ca24fca6f731a5e
Victor <deVictorNeves@gmail.com>
2025-11-16 18:31:16 -0300

    feat: add score and lives to GUI

--- scripts/game_ui.gd
+++ scripts/game_ui.gd
@@ -2,8 +2,7 @@ extends Control
 
 @onready var score_value : Label = %"Score Value"
 @onready var lives_value: Label = %"Lives Value"
-@onready var SCORE : int = GLOBAL_VARIABLES.score;
-@onready var PLAYER_LIVES : int = GLOBAL_VARIABLES.PLAYER_LIVES;
 
 func _process(_delta: float) -> void:
-	score_value.text = str(SCORE);
+	score_value.text = str(GLOBAL_VARIABLES.score);
+	lives_value.text = str(GLOBAL_VARIABLES.PLAYER_LIVES);
